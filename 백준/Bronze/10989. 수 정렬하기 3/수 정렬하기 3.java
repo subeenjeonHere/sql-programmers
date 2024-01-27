9701fedee457dcd92f398cdcaa44f1492a1bf94d
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-01-27 12:20:23 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1808 ms, Memory: 385616 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,25 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.Buffer;
 import java.util.Arrays;
+
+/**
+ * sec. Counting Sort로 풀기
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] cnt = new int[100001];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        // cnt 배열에 값 저장 동시에 cnt 카운팅
+        // b/c 배열의 인덱스를 사용할 것이므로
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            cnt[num]++;
         }
-        Arrays.sort(arr);
-
         StringBuilder sb = new StringBuilder();
-
-        for (int ele : arr) {
-            sb.append(ele).append('\n');
+        // N.B.: cnt 순회하면서 cnt idx의 원소가 0이 될 때까지 출력
+        for (int i = 0; i < cnt.length; i++) {
+            while (cnt[i] > 0) {
+                sb.append(i + "\n");
+                cnt[i]--;
+            }
         }
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
