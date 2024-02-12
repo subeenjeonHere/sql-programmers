c61c3610b557c02bcf4ef9909d281d2a0cebe72d
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-13 07:25:43 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if(n%7==0){
-            answer = (n/7);
-        }else{
-            answer = (n/7)+1;
-        }
+        answer = (n%7==0) ? (n/7) : (n/7)+1;
         return answer;
     }
 }
