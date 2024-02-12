058610520f1d1ea3a2c3ddf805686580c800a4c2
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-12 18:29:37 +0900

    [level 0] Title: 문자열 뒤집기, Time: 2.10 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,17 +3,14 @@ import java.util.*;
 class Solution {
     public String solution(String my_string) {
         
+        
         String str = my_string;
+        String answer ="";
         String[] arr = new String[str.length()];
         
         for(int i=0; i < str.length(); i++){
-            arr[i] = String.valueOf(str.charAt(str.length()-i-1));
-        }
-    
-        StringBuffer sb = new StringBuffer();
-        for(int i=0; i<arr.length; i++){
-            sb.append(arr[i]);
-        }
-        return sb.toString();
+            answer += String.valueOf(str.charAt(str.length()-i-1));
+        }        
+        return answer;
     }
 }
