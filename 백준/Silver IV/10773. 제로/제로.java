7ae143d4071f302aff17b431fba183cba70fe1d2
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-15 12:57:05 +0900

    [Silver IV] Title: 제로, Time: 232 ms, Memory: 24120 KB -BaekjoonHub

--- 백준/Silver IV/10773. 제로/제로.java
+++ 백준/Silver IV/10773. 제로/제로.java
@@ -1,27 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int k = Integer.parseInt(br.readLine());
-		Stack<Integer> stk = new Stack<>();
-		
-		//Pushing element on the top of the stack
-		
-		for(int i = 0; i<k; i++) {			
-			int e = Integer.parseInt(br.readLine());
-			
-			if(e!=0) {stk.push(e);}
-			else {stk.pop();	}
-		}
-			int sum =0;
-			for(int n : stk) {
-				sum += n;
-			}	
-			System.out.println(sum);
-		}
-	}
+        int k = Integer.parseInt(br.readLine());
+
+        //Declare 스택
+        Stack<Integer> stack = new Stack<>();
+
+        /**
+         * for문으로 stack에 값 입력
+         * 만약, "0"이면 가장 최근에 stack에 입력된 값 pop, 0일 경우, !stack.isEmpty 보장
+         * 아니면, push
+         */
+        for (int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        /**
+         * print: 스택의 모든 원소 합
+         */
+        int answer = 0;
+        for (int element : stack) {
+            answer += element;
+
+        }
+        System.out.println(answer);
+    }
+}
