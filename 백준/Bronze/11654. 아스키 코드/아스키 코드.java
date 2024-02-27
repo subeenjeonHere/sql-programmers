45b9e00debdc69d0b05879702d9efa79c98d05a1
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-02-27 15:31:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String myString = sc.next();
+
+        char myChar = myString.charAt(0);
+        System.out.println((int) myChar);
+
+
+    }
+}
