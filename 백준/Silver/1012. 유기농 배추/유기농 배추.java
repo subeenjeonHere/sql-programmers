647c72ba97910c9f7ba078453bd04ff8f5683efe
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-03-16 09:22:18 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 17240 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,128 +1,108 @@
-//package baek_joon.class_3.유기농배추;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.annotation.ElementType;
-
+import java.io.LineNumberReader;
+import java.util.Formattable;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
- * @Point - 시작 노드 설정
- * - 4방 탐색
- * - 4방 탐색 결과 방문 체크 및 DFS 호출
- * - 4방 탐색하면서 1있으면 dfs 재귀호출 지속, 1 없다면 종료
- * - 상하 좌우로 탐색하기에 1이 여러 개 있다면 원래 노드로 복귀해야 함
- * - 단지 탐색 종료 후 카운팅 어디서 누적?
+ * @Subject - BFS 풀이
+ */
+
+/**
+ * @Point 왜 그래프 n+2, m+2로 해줘야 하는지
+ * 큐 BFS 4방탐색
+ * 예제 2에서 4,0 탐색 안 됨
+ * 4방탐색 continue 조건 설정
  */
 public class Main {
-    static int n, m;
-    static int[][] g;
-    static boolean[][] visited;
-    //4방 탐색 델타 배열
-    static int[] node;
+    // 가로 m, 세로 n, 노드 k
+    static int m, n, k;
+    static int[][] graph;
+    static boolean[][] visit;
     static int count;
-    static int x, y;
 
     public static void main(String[] args) throws IOException {
-        // 인접행렬 할당
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        //테케
         int tc = Integer.parseInt(br.readLine());
-//        for (int w = 0; w < tc; w++) {
+        //가로길이, 세로길이, 노드
+        for (int i = 1; i <= tc; i++) {
 
-        for (int t = 1; t <= tc; t++) {
             String[] info = br.readLine().split(" ");
             m = Integer.parseInt(info[0]);
             n = Integer.parseInt(info[1]);
-            int k = Integer.parseInt(info[2]);
+            k = Integer.parseInt(info[2]);
 
-            g = new int[51][51];
-
-            //노드 저장
-            node = new int[51];
             // 인접 행렬 할당
-            for (int i = 1; i <= k; i++) {
+            graph = new int[51][51];
+            visit = new boolean[51][51];
+
+            //인접 행렬 할당 2
+            for (int j = 1; j <= k; j++) {
                 String[] ab = br.readLine().split(" ");
                 int x = Integer.parseInt(ab[0]);
                 int y = Integer.parseInt(ab[1]);
-                g[x][y] = 1;
+                graph[x][y] = 1;
             }
-
-//            System.out.println("====");
-            //인접 행렬 출력
-//            for (int i = 0; i < g.length; i++) {
-//                for (int j = 0; j < g[i].length; j++) {
-//                    System.out.print(g[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println("====");
-
-            // 방문 배열 할당
-            visited = new boolean[51][51];
-            int result = 0;
-            //시작할 노드 찾기 (0,0)
-            // 1이 있으면 dfs Call
-            for (int i = 0; i < 51; i++) {
-                for (int j = 0; j < 51; j++) {
-                    if (g[i][j] == 1 && !visited[i][j]) {
-                        dfs(i, j, g, visited);
-                        result++;
+            for (int x = 0; x < graph.length; x++) {
+                for (int y = 0; y < graph[x].length; y++) {
+                    if (graph[x][y] == 1 && !visit[x][y]) {
+                        count++;
+                        bfs(x, y, visit, graph);
                     }
                 }
             }
-            System.out.println(result);
+            System.out.println(count);
+            count = 0;
         }
-//        System.out.println();
     }
 
+    //BFS 시작
+    private static void bfs(int x, int y, boolean[][] visit, int[][] graph) {
 
-//}
 
-
-    //DFS 동작
-    private static void dfs(int x, int y, int[][] g, boolean[][] visited) {
-        //방문 체크
-        visited[x][y] = true;
-        // 상하 좌우
+        //델타 4방 탐색
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-//        System.out.println();
-//        System.out.println("Node: " + g[x][y]);
-//        System.out.println("x좌표: " + x + " / y좌표: " + y);
+        //큐 배열 선언
+        Queue<int[]> queue = new LinkedList<>();
+        //큐에 시작노드 좌표 삽입
+        queue.add(new int[]{x, y});
 
-        // 4방 탐색하며 인접 노드 탐색
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
+        //방문 체크
+        visit[x][y] = true;
+        //BFS 반복
+        while (!queue.isEmpty()) {
 
-            if (nx < 0 || ny < 0 || x > g.length || y > g[0].length) {
-                continue;
+            //좌표배열 있어야 함
+            int[] current = queue.poll();
+            int nowX = current[0];
+            int nowY = current[1];
+
+            //4방 탐색 인접행렬
+            for (int d = 0; d < 4; d++) {
+                int nx = nowX + dx[d];
+                int ny = nowY + dy[d];
+
+                if (nx < 0 || ny < 0 || nx >= graph.length || ny >= graph[0].length) {
+                    continue;
+                }
+
+
+                // 큐에 방문하지 않았고, 4방 탐색 좌표가 1이라면
+                if (graph[nx][ny] == 1 && !visit[nx][ny]) {
+                    //방문 처리
+                    visit[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
             }
-
-//            System.out.println(nx + " !GO! " + ny);
-//            System.out.println("Graph:  " + g[nx][ny]);
-
-            if (g[nx][ny] == 0) {
-                continue;
-            }
-
-            // 방문하지 않았고, 노드 있으면 방문체크, dfs recursive call
-            if (!visited[nx][ny] && g[nx][ny] == 1) {
-                visited[nx][ny] = true;
-                dfs(nx, ny, g, visited);
-                // 여기서 단지 탐색 후 또 다시 반복됨
-            }
-
         }
 
     }
 }
-
-
-/**
- * 배추가 심어져있는 곳만 인접 행렬에 할당해주어야 함
- * 노드의 시작점을 따로 지정해주지 안았으므로, 찾아내고 시작 노드를 설정해야 한다
- * 메인 메소드에서 graph를 탐색하며 1이 발견되면, 해당 노드를 기준으로 dfs를 호출한다
- */
