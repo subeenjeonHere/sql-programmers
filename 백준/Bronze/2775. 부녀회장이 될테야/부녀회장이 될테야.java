05c44d4e13eed24cedc81d908b270ebc43a52e8b
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-04-01 07:12:09 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 116 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,70 +1,45 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.SimpleTimeZone;
 
 public class Main {
-    static int[] s3array;
-    static int k;
     static int n;
+    static int a, b;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        // 0층 1- 14호 사람 채운다
-        int[] array = new int[15];
-//        for (int i = 0; i < array.length; i++) {
-//            array[i] = i + 1;
-//        }
+        n = Integer.parseInt(br.readLine());
 
-        //1층 3호면 0층의 1~3호까지 누적합
-        //2층 3호면 1층 1~3호까지 누적합
-        //3층 3호면 2층 1~3호까지의 누적합
-        /**
-         * a/t
-         * 3층 3호 사람 수를 구하기 위해 누적합을 0층부터 차례로 구해야 하는지
-         * 호근 다른 방법으로 구할 수 있는지 체크
-         */
+        for (int i = 0; i < n; i++) {
+            a = Integer.parseInt(br.readLine());
+            b = Integer.parseInt(br.readLine());
 
-        int tc = sc.nextInt();
-        s3array = new int[15];
-        for (int i = 1; i <= tc; i++) {
+            //배열 초기값
+            dp = new int[a + 2][b + 1];
 
-            int k = sc.nextInt();
-            int n = sc.nextInt();
-            //1층 3호일 때 1, 2, 3
-            //3층 3호일 때 123 / 1 123 / 2 123
-
-            int start = 1;
-
-            for (int p = 0; p < array.length; p++) {
-                array[p] = p + 1;
+            for (int p = 0; p < dp[0].length; p++) {
+                dp[0][p] = p + 1;
             }
-            s3array[0] = array[0];
+            for (int q = 0; q < dp.length; q++) {
+                dp[q][0] = 1;
+            }
 
-            while (start <= k) {
+            for (int p = 1; p < dp.length; p++) {
+                for (int q = 1; q < dp[0].length; q++) {
 
-//                System.out.println("START: " + start);
-                for (int x = 1; x <= array.length; x++) {
-                    if (x > n) {
-                        break;
-                    }
-                    s3array[x] = s3array[x - 1] + array[x];
+                    dp[p][q] = dp[p][q - 1] + dp[p - 1][q];
                 }
-                array = s3array;
-
-//                System.out.print("누적합::" + Arrays.toString(s3array));
-
-                //누적합으로 반복하니 sb에 전체 추가됨, 3층 10호일 땐 3층 10호만 count 해야하는데 1,2층까지 같이 count
-
-                start++;
             }
-            sb.append(s3array[n - 1]).append("\n");
+            sb.append(dp[a][b - 1]).append("\n");
         }
-        System.out.println(sb.toString().trim());
+        System.out.print(sb.toString().trim());
 
     }
 }
